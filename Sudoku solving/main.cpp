476b41ca6ce9611cba13283933ef680b2d4bf57c
Hongjun Yun <andylang8445@gmail.com>
2019-03-08 22:27:16 -0500

    Initial Commit

---
+++ Sudoku solving/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sudoku solving
+//
+//  Created by HongjunYun on 2019-03-08.
+//  Copyright © 2019 HongjunYun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
